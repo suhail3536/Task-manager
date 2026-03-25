7969984422fe56fba6395b1e728388838b9b67d2
Suhail <shab35360@gmail.com>
2026-03-25 14:16:14 +0530

    update

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const auth = require("../middleware/authMiddleware");
+const authMiddleware = require('../middleware/authMiddleware');
 
 const {
   createTask,
